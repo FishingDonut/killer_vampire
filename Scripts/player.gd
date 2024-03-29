6290ed397d35eb85f93797b51b20305dd0939583
FishingDonut <protronsdimi@gmail.com>
2024-03-29 15:48:13 -0300

    update script player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,28 +8,53 @@ class_name Player
 
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed_walk = 10.0
 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
 var direction := Vector2.ZERO
+
+
+var is_death := false
+var is_hurt := false
+
 
 func _ready():
 	Global.player = self
 	remote_camera.remote_path = Global.camera.get_path()
 
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 
 	_move_direction()
+	_state()
+	_scale_direction()
 	move_and_slide()
 
 
 func _move_direction() -> void:
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized().round()
 	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity = direction * speed_walk
+		return
+	
+	velocity.x = move_toward(velocity.x, 0, speed_walk)
+	velocity.y = move_toward(velocity.y, 0, speed_walk)
+
+
+func _state() -> void:
+	z_index = position.y
+	var state = "idle"
+	
+	if direction != Vector2.ZERO:
+		state = "walk"
+	
+	if anim.name != state:
+		anim.play(state)
+
+
+func _scale_direction() -> void:
+	if direction.x != 0:
+		sprite.scale.x = direction.x
