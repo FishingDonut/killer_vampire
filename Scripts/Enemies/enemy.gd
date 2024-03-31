183c10c1fbe9868b93db020bb1a15e512344c732
FishingDonut <protronsdimi@gmail.com>
2024-03-31 11:50:53 -0300

    enemy take_damage

---
+++ Scripts/Enemies/enemy.gd
@@ -0,0 +1,30 @@
+class_name Enemy
+extends CharacterBody2D
+
+
+@export var move_speed: float
+
+
+var direction: Vector2
+var max_hp: float
+
+signal take_damage(damage: float)
+
+func _ready():
+	take_damage.connect(_take_damage)
+
+func _physics_process(delta):
+	direction = (Global.player.position - position).round().normalized()
+
+	if direction:
+		velocity = direction * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.y = move_toward(velocity.y, 0, move_speed)
+
+	move_and_slide()
+
+func _take_damage(damage):
+	max_hp -= damage
+	if max_hp <= 0:
+		queue_free()
