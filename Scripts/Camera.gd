54535efff463a356a0b526ff94a2a577e5f65700
FishingDonut <protronsdimi@gmail.com>
2024-04-28 19:02:12 -0300

    update camera

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,8 +1,12 @@
 extends Node2D
 class_name Camera
 
+@onready var camera = $Camera
+
+@export var zoom_camera: float = 1.0
 
 func _ready():
+	camera.zoom = Vector2(zoom_camera, zoom_camera)
 	Global.camera = self
 
 
